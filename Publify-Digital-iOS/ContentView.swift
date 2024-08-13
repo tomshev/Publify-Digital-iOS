e3df652f8630756ae84d9d97a5e61823ebd25b43
tomshev <shevcov7@icloud.com>
2024-08-14 02:17:09 +0300

    Initial Commit

---
+++ Publify-Digital-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Publify-Digital-iOS
+//
+//  Created by Tom Sevcov on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Publify-Digital-iOS/Publify_Digital_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Publify_Digital_iOSApp.swift
+//  Publify-Digital-iOS
+//
+//  Created by Tom Sevcov on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Publify_Digital_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
